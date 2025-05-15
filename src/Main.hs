9fffb714086bcc7dbea87d650e976583b781f167
Emily Sánchez Orozco <emilyjohana2304@gmail.com>
2025-05-14 18:52:27 -0600

    Cambios en el registro
    Se guarda en json
    nombre de usuario unico
    asteriscos para el pin

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,111 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
-import System.IO (hSetEcho, hGetEcho, stdin, stdout, hFlush)
+import GHC.Generics (Generic)
+import System.IO (hSetEcho, hGetEcho, stdin, stdout, hFlush, getChar)
 import Data.Char (isSpace)
 import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text as T
+import Data.List (find)
 import Crypto.Hash (hash, SHA256(..), Digest)
 import Data.ByteString.Base16 (encode)
 import System.Process (callCommand)
-import System.Directory (doesFileExist, removeFile)
-import Control.Monad (when)
-import Data.List (lines, break)
+import System.Directory (doesFileExist)
 import System.Exit (exitSuccess)
+import Data.Aeson (FromJSON, ToJSON, decodeFileStrict, encodeFile)
+
+-- Estructura de usuario
+data UserInfo = UserInfo
+  { username :: String
+  , pinHash :: String
+  } deriving (Show, Generic)
+
+instance FromJSON UserInfo
+instance ToJSON UserInfo
+
+-- Archivo donde se guarda la información de usuarios
+userFile :: FilePath
+userFile = "users.json"
+
+-- Hashear el PIN
+hashPIN :: String -> String
+hashPIN pin =
+  let digest = hash (BC.pack pin) :: Digest SHA256
+      str = show digest
+      bytes = BC.pack str
+  in BC.unpack $ encode bytes
+
+-- Leer input oculto con asteriscos
+getHiddenInput :: IO String
+getHiddenInput = do
+  hSetEcho stdin False
+  input <- loop ""
+  hSetEcho stdin True
+  putStrLn ""
+  return input
+  where
+    loop acc = do
+      c <- getChar
+      case c of
+        '\n' -> return acc
+        '\DEL' -> if null acc
+                    then loop acc
+                    else do putStr "\b \b"; loop (init acc)
+        _ -> do
+          putChar '*'
+          loop (acc ++ [c])
+
+-- Cargar usuarios desde JSON
+loadUsers :: IO [UserInfo]
+loadUsers = do
+  exists <- doesFileExist userFile
+  if exists
+    then do
+      mUsers <- decodeFileStrict userFile
+      return (maybe [] id mUsers)
+    else return []
+
+-- Guardar usuarios en JSON
+saveUsers :: [UserInfo] -> IO ()
+saveUsers = encodeFile userFile
+
+-- Registro de usuario con validación
+registerUser :: IO ()
+registerUser = do
+  users <- loadUsers
+  putStrLn "Por favor, introduce un nombre de usuario:"
+  username' <- getLine
+  if any (\u -> username u == username') users
+    then putStrLn "Ese nombre de usuario ya está registrado."
+    else do
+      putStrLn "Define un PIN:"
+      pin1 <- getHiddenInput
+      putStrLn "Confirma tu PIN:"
+      pin2 <- getHiddenInput
+      if pin1 == pin2
+        then do
+          let newUser = UserInfo username' (hashPIN pin1)
+          saveUsers (users ++ [newUser])
+          putStrLn "Usuario registrado exitosamente."
+        else
+          putStrLn "Los PINs no coinciden."
+
+-- Validar ingreso de usuario
+requestPIN :: IO Bool
+requestPIN = do
+  users <- loadUsers
+  putStrLn "Nombre de usuario:"
+  enteredUsername <- getLine
+  putStrLn "PIN:"
+  enteredPIN <- getHiddenInput
+  case find (\u -> username u == enteredUsername) users of
+    Just user ->
+      if hashPIN enteredPIN == pinHash user
+        then return True
+        else putStrLn "PIN incorrecto." >> return False
+    Nothing -> putStrLn "Usuario no encontrado." >> return False
 
 -- Menú principal
 showMenu :: IO ()
@@ -21,93 +117,7 @@ showMenu = do
   putStr "Seleccione una opción: "
   hFlush stdout
 
--- Ocultar PINs Preguntar si así o con asteriscos ----------------------------
-withEcho :: Bool -> IO String -> IO String
-withEcho echo action = do
-  old <- hGetEcho stdin
-  hSetEcho stdin echo
-  x <- action
-  hSetEcho stdin old
-  return x
-
--- Usuario
-data UserInfo = UserInfo { username :: String, pinHash :: String }
-
--- Archivo CAMBIAR Y VER CUAL VAMOS A USAR ---------------
-userFile :: FilePath
-userFile = "users.txt"
-
--- Hashear el PIN TEMPORALLLLLLLLLLLLLLLLLLLLLLLLLL -----------------
-hashPIN :: String -> String
-hashPIN pin = 
-  let digest = hash (BC.pack pin) :: Digest SHA256
-      str = show digest
-      bytes = BC.pack str
-  in BC.unpack $ encode bytes
-
--- Registrar un usuario
-registerUser :: IO ()
-registerUser = do
-  putStrLn "Bienvenido al Administrador de Contraseñas."
-  putStrLn "Por favor, introduce un nombre de usuario:"
-  username' <- getLine
-  putStrLn "Por favor, define un PIN para proteger tu acceso:"
-  pin1 <- withEcho False getLine
-  putStrLn "\nConfirma tu PIN:"
-  pin2 <- withEcho False getLine
-  putStrLn "" 
-  
-  if pin1 == pin2
-    then do
-      let pinHashed = hashPIN pin1
-      appendFile userFile (username' ++ ":" ++ pinHashed ++ "\n")
-      putStrLn $ "Usuario '" ++ username' ++ "' registrado correctamente."
-    else do
-      putStrLn "Los PINs no coinciden."
-
-
-splitOn :: Char -> String -> [String]
-splitOn delimiter text = case break (== delimiter) text of
-  (prefix, "")    -> [prefix]
-  (prefix, suffix) -> prefix : splitOn delimiter (drop 1 suffix)
-
--- Buscar un usuario por nombre
-findUser :: String -> [[String]] -> Maybe (String, String)
-findUser usernameToFind users =
-  foldr (\(u:ph:_) acc -> if u == usernameToFind then Just (u, ph) else acc) Nothing users
-
--- Verificar el PIN
-requestPIN :: IO Bool
-requestPIN = do
-  putStrLn "Por favor, ingresa tu nombre de usuario:"
-  enteredUsername <- getLine
-  putStrLn "Por favor, ingresa tu PIN para acceder:"
-  enteredPIN <- withEcho False getLine
-  putStrLn ""
-
-  fileExists <- doesFileExist userFile
-  if fileExists
-    then do
-      contents <- readFile userFile
-      let users = map (splitOn ':') (lines contents)
-      case findUser enteredUsername users of
-        Just (_, storedPINHash) -> return (hashPIN enteredPIN == trim storedPINHash)
-        Nothing -> do
-          putStrLn "Nombre de usuario no encontrado."
-          return False
-    else do
-      putStrLn "No se encontró información de usuario, primero debes registrarte."
-      return False
-
--- Trim
-trim :: String -> String
-trim = reverse . dropWhile isSpace . reverse
-
--- FCopiar texto al portapapeles 
-copyToClipboard :: String -> IO ()
-copyToClipboard text = callCommand $ "echo " ++ text ++ " | clip"
-
--- Menú
+-- Menú selección
 handleMenuSelection :: IO ()
 handleMenuSelection = do
   option <- getLine
@@ -117,14 +127,11 @@ handleMenuSelection = do
       if accessGranted
         then do
           putStrLn "\nInicio de sesión exitoso!"
-          -- AGREGAR LO DE LAS CONTRASEÑAASSSSSSSSSSS ----------------
-          putStrLn "Pendiente............."
-          showMenu
-          handleMenuSelection
-        else do
-          putStrLn "\nError en las credenciales"
-          showMenu
-          handleMenuSelection
+          -- Aquí va la parte 2.2 (Gestión de contraseñas)
+          putStrLn "Funcionalidad de contraseñas aún no implementada."
+        else putStrLn "Acceso denegado."
+      showMenu
+      handleMenuSelection
     "2" -> do
       registerUser
       showMenu
@@ -133,20 +140,12 @@ handleMenuSelection = do
       putStrLn "Saliendo del sistema..."
       exitSuccess
     _ -> do
-      putStrLn "Opción no válida, por favor intente nuevamente."
+      putStrLn "Opción no válida. Intente nuevamente."
       showMenu
       handleMenuSelection
 
--- Función principal 
+-- Función principal
 main :: IO ()
 main = do
-  fileExists <- doesFileExist userFile
-  if not fileExists
-    then do
-      putStrLn "Primera vez que ingresas a nuestro sistema, Bienvenido."
-      registerUser
-      showMenu
-      handleMenuSelection
-    else do
-      showMenu
-      handleMenuSelection
+  showMenu
+  handleMenuSelection
