af1e0b34a3a4abcbd845ca036433cab65ab896ad
AndyBS1 <andy230505@gmail.com>
2025-05-15 08:02:06 -0600

    Funcion de Agregar

--- src/GestionDeContrasenas.hs
+++ src/GestionDeContrasenas.hs
@@ -10,109 +10,149 @@ import GHC.Generics (Generic)
 import Data.Aeson
 import Data.IORef (readIORef)
 import SesionActual (currentUser)
+import qualified Data.ByteString.Lazy as B
+import Control.Monad (when)
+import System.Directory (doesFileExist)
 
 data Credencial = Credencial {
     titulo :: String,
     usuario :: String,
     password :: String
-} deriving (Show, Generic)
+} deriving (Show, Generic, Eq)
 
--- Instancia de Aeson para Credencial
-instance ToJSON Credencial where
-  toJSON (Credencial titulo usuario password) =
-    object ["titulo" .= titulo, "usuario" .= usuario, "password" .= password]
-
-instance FromJSON Credencial where
-  parseJSON = withObject "Credencial" $ \v ->
-    Credencial <$> v .: "titulo"
-               <*> v .: "usuario"
-               <*> v .: "password"
+instance ToJSON Credencial
+instance FromJSON Credencial
 
 data Cuenta = Cuenta
   { cuenta :: String,
     credenciales :: [Credencial]  
-  } deriving (Show, Generic)
+  } deriving (Show, Generic, Eq)
 
--- Instancia de Aeson para Cuenta
-instance ToJSON Cuenta where
-  toJSON (Cuenta cuenta credenciales) =
-    object ["cuenta" .= cuenta, "credenciales" .= credenciales]
-
-instance FromJSON Cuenta where
-  parseJSON = withObject "Cuenta" $ \v ->
-    Cuenta <$> v .: "cuenta"
-           <*> v .: "credenciales"
+instance ToJSON Cuenta
+instance FromJSON Cuenta
 
 passwordFile :: FilePath
-passwordFile = "passwords.json"
+passwordFile = "cuentas.json"
+
+agregarCredencialACuenta :: String -> Credencial -> [Cuenta] -> [Cuenta]
+agregarCredencialACuenta nombreUsuario nuevaCredencial =
+  map actualizarCuenta
+  where
+    actualizarCuenta cuenta@(Cuenta nombre creds)
+      | nombre == nombreUsuario = Cuenta nombre (nuevaCredencial : creds)
+      | otherwise = cuenta
 
 iniciarGestion  :: IO ()
 iniciarGestion  = do
     hSetBuffering stdout NoBuffering
-    putStrLn "\n--------------------------------"
-    putStrLn "Gestion de Contrasenas"
-    putStrLn "1. Consultar todas"
-    putStrLn "2. Consultar por servicio"
-    putStrLn "3. Consultar por cuenta"
-    putStrLn "4. Agregar"
-    putStrLn "5. Modificar"
-    putStrLn "6. Eliminar"
-    putStrLn "7. Salir"
-    putStr "Seleccione una opción (1-7): "
-    opcion <- getLine
-    
-    case opcion of
-        "1" -> do
-            putStrLn "\n--------------------------------"
-            -- Mostrar todas las cuentas
-            iniciarGestion
 
-        "2" -> do
-            putStrLn "\n--------------------------------"
-            putStrLn "Ingrese un servicio: "
-            texto <- getLine
+    maybeUsuario <- readIORef currentUser
+    case maybeUsuario of
+      Nothing -> putStrLn "ERROR. No se ha encontrado una sesión activa"
+      Just enteredUsername -> do
+        let rutaArchivo = "cuentas.json"
+        existe <- doesFileExist rutaArchivo
+        cuentas <- if existe
+                    then do
+                      contenido <- B.readFile rutaArchivo
+                      case decode contenido of
+                        Just cs -> return cs
+                        Nothing -> return []
+                    else return []
 
-            iniciarGestion
+        let yaExisteCuenta = any (\(Cuenta nombre _) -> nombre == enteredUsername) cuentas
 
-        "3" -> do
-            putStrLn "\n--------------------------------"
-            putStrLn "Ingrese el nombre de la cuenta: "
-            texto <- getLine
+        when (not yaExisteCuenta) $ do
+          let nuevaCuenta = Cuenta enteredUsername []
+              cuentasActualizadas = nuevaCuenta : cuentas
+          B.writeFile rutaArchivo (encode cuentasActualizadas)
+          putStrLn $ "Cuenta creada para el usuario: " ++ enteredUsername
+
+        -- Menu
+        putStrLn "\n--------------------------------"
+        putStrLn "Gestion de Contrasenas"
+        putStrLn "1. Consultar todas"
+        putStrLn "2. Consultar por servicio"
+        putStrLn "3. Consultar por cuenta"
+        putStrLn "4. Agregar"
+        putStrLn "5. Modificar"
+        putStrLn "6. Eliminar"
+        putStrLn "7. Salir"
+        putStr "Seleccione una opción (1-7): "
+        opcion <- getLine
+
+        case opcion of
+            "1" -> do
+                putStrLn "\n--------------------------------"
+                -- Mostrar todas las cuentas
+                iniciarGestion
+
+            "2" -> do
+                putStrLn "\n--------------------------------"
+                putStrLn "Ingrese un servicio: "
+                texto <- getLine
+
+                iniciarGestion
+
+            "3" -> do
+                putStrLn "\n--------------------------------"
+                putStrLn "Ingrese el nombre de la cuenta: "
+                texto <- getLine
+                
+                iniciarGestion
+                
+            "4" -> do
+                putStrLn "\n--------------------------------"
+                putStrLn "Ingrese un servicio: "
+                servicio <- getLine
+
+                putStrLn "Ingrese el usuario: "
+                usuario <- getLine
+
+                putStrLn "Ingrese su contrasena: "
+                password <- getLine
+                
+                let nuevaCredencial = Credencial servicio usuario password
+
+                let yaExisteServicio = any (\(Cuenta nombre creds) ->
+                                      nombre == enteredUsername &&
+                                      any (\c -> titulo c == servicio) creds
+                                  ) cuentas
+
+                if yaExisteServicio
+                  then putStrLn "Ya existe una credencial para ese servicio."
+                  else do
+                    let cuentasActualizadas =
+                          map (\cuenta@(Cuenta nombre creds) ->
+                                if nombre == enteredUsername
+                                  then Cuenta nombre (nuevaCredencial : creds)
+                                  else cuenta
+                              ) cuentas
+
+                    B.writeFile rutaArchivo (encode cuentasActualizadas)
+                    putStrLn "Credencial agregada exitosamente."
+
+                iniciarGestion
+                
+            "5" -> do
+                putStrLn "\n--------------------------------"
+                putStrLn "Ingrese un servicio: "
+                servicio <- getLine
+
+                putStrLn "Ingrese el dato a modificar (usuario/contraseña): "
+                dato <- getLine
+                
+                iniciarGestion
+
+            "6" -> do
+                putStrLn "\n--------------------------------"
+                putStrLn "Ingrese un servicio: "
+                servicio <- getLine
+                
+                iniciarGestion
+
+            "7" -> putStrLn "Saliendo del programa..."
             
-            iniciarGestion
-            
-        "4" -> do
-            putStrLn "\n--------------------------------"
-            putStrLn "Ingrese un servicio: "
-            servicio <- getLine
-
-            putStrLn "Ingrese el usuario: "
-            usuario <- getLine
-
-            putStrLn "Ingrese su contraseña: "
-            password <- getLine
-            
-            iniciarGestion
-            
-        "5" -> do
-            putStrLn "\n--------------------------------"
-            putStrLn "Ingrese un servicio: "
-            servicio <- getLine
-
-            putStrLn "Ingrese el dato a modificar (usuario/contraseña): "
-            dato <- getLine
-            
-            iniciarGestion
-
-        "6" -> do
-            putStrLn "\n--------------------------------"
-            putStrLn "Ingrese un servicio: "
-            servicio <- getLine
-            
-            iniciarGestion
-
-        "7" -> putStrLn "Saliendo del programa..."
-        
-        _ -> do
-            putStrLn "Opción no válida. Intente nuevamente."
-            iniciarGestion
+            _ -> do
+                putStrLn "Opción no válida. Intente nuevamente."
+                iniciarGestion
